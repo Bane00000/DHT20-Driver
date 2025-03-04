1e310156ce9688f4302db6672762f814bfe25e9b
Bane <galovicbranislav@gmail.com>
2025-03-04 19:16:55 +0100

    Finished gpio_write_pin function, started gpio_init

--- Drivers/GPIO/Inc/gpio_stm32f4xx.h
+++ Drivers/GPIO/Inc/gpio_stm32f4xx.h
@@ -8,6 +8,33 @@
 #ifndef GPIO_INC_GPIO_STM32F4XX_H_
 #define GPIO_INC_GPIO_STM32F4XX_H_
 
+#include "stm32f4xx.h"
+
+typedef struct
+{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_Speed;
+	uint8_t GPIO_PuPd;
+	uint8_t GPIO_OType;
+	uint8_t GPIO_AltFun;
+} GPIO_Config_TypeDef;
+
+typedef struct
+{
+	GPIO_TypeDef GPIOx;
+
+	GPIO_Config_TypeDef GPIO_Config;
+
+} GPIO_Handle_TypeDef;
+
+void gpio_init(void);
+
+void gpio_deinit(void);
+
+void gpio_read(void);
+
+void gpio_write_pin(GPIO_TypeDef *GPIOx, uint8_t Pin, uint8_t data);
 
 
 #endif /* GPIO_INC_GPIO_STM32F4XX_H_ */
--- Drivers/GPIO/Src/gpio_stm32f4xx.c
+++ Drivers/GPIO/Src/gpio_stm32f4xx.c
@@ -5,4 +5,42 @@
  *      Author: Bane
  */
 
+#include "gpio_stm32f4xx.h"
 
+void gpio_init(GPIO_Handle_TypeDef *GPIOHandle)
+{
+	// GPIO clock activation
+	if (GPIOHandle->GPIOx == GPIOA)
+	{
+		RCC_AHB1ENR_GPIOAEN();
+	}
+	else if (GPIOHandle->GPIOx == GPIOD)
+	{
+		RCC_AHB1ENR_GPIODEN();
+	}
+
+	if (GPIOHandle->GPIO_Config->GPIO_PinMode == GPIO_MODE_INPUT)
+	{
+		GPIOHandle->GPIOx->MODER &= ~(3u << (PinNumber*2));
+	}
+	else if (GPIOHandle->GPIO_Config->GPIO_PinMode == GPIO_MODE_OUTPUT)
+	{
+		GPIOHandle->GPIOx->MODER |= (1u << (PinNumber*2));
+	}
+}
+
+void gpio_deinit(void);
+
+void gpio_read(void);
+
+void gpio_write_pin(GPIO_TypeDef *GPIOx, uint8_t Pin, uint8_t data)
+{
+	if (data == 1)
+	{
+		GPIOx->ODR |= (1u << Pin);
+	}
+	else if (data == 0)
+	{
+		GPIOx->ODR &= ~(1u << Pin);
+	}
+}
