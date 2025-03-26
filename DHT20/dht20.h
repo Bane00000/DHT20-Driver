fb6cf568e9403084f8a4a8f1d634a627933a78a0
Branislav Galović <galovicbranislav@gmail.com>
2025-03-26 15:39:20 +0100

    Remove magic values, add functions for humidity/temperature calculation

--- DHT20/dht20.h
+++ DHT20/dht20.h
@@ -11,19 +11,29 @@
 #include "stm32f4xx_hal.h"
 
 #define DHT20_DEVICE_ADDRESS	(0x38 << 1)
+
+#define STATUS_WORD				0x18
+#define INIT_REG_1				0x1B
+#define INIT_REG_2				0x1C
+#define INIT_REG_3				0x1E
+#define MEASUREMENT_COMMAND			0XAC
+#define PARAMETER_ONE				0x33
+#define PARAMETER_TWO				0x00
+#define STATUS_WORD_BIT_SEVEN_MASK		0x80
+
 #define ONE_BYTE		1
 #define THREE_BYTES		3
 #define SIX_BYTES		6
 
 typedef struct
 {
-	I2C_HandleTypeDef *i2c_handle;		/* I2C handle */
-	uint8_t addres;				/* Address of DHT20 sensor */
-	uint8_t status_word;			/* First byte received to check if it's equal to 0x18 */
-	uint8_t readBuffer[6];			/* 6 bytes for temperature and humidity */
-	uint32_t data;				/* Raw data for temperature and humidity */
-	float temperature;			/* Temperature value */
-	float humidity;				/* Humidity value */
+	I2C_HandleTypeDef *i2c_handle;			/* I2C handle */
+	uint8_t addres;					/* Address of DHT20 sensor */
+	uint8_t status_word;				/* First byte received to check if it's equal to 0x18 */
+	uint8_t readBuffer[6];				/* 6 bytes for temperature and humidity */
+	uint32_t data;					/* Raw data for temperature and humidity */
+	float temperature;				/* Temperature value */
+	float humidity;					/* Humidity value */
 } DHT20_t;
 
 void dht20_init(DHT20_t *sensor, I2C_HandleTypeDef *i2c);
@@ -35,6 +45,12 @@ static void dht20_read(DHT20_t *sensor, uint8_t *rBuffer, uint16_t size);
 void dht20_check_status_word(DHT20_t *sensor);
 uint8_t dht20_read_measurement(DHT20_t *sensor);
 void dht20_parse_data(DHT20_t *sensor);
+
+uint32_t humidity_calculation(DHT20_t *sensor);
+uint32_t temperature_calculation(DHT20_t *sensor);
+
+float humidity_formula(DHT20_t *sensor);
+float temperature_formula(DHT20_t *sensor);
 
 void dht20_sensor_read(DHT20_t *sensor);
 
