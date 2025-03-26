991ed164840ec4a86ad14ee9b72a502a3acf26ba
Branislav Galović <galovicbranislav@gmail.com>
2025-03-26 16:07:51 +0100

    Update dht20.h

--- DHT20/dht20.h
+++ DHT20/dht20.h
@@ -27,23 +27,23 @@
 
 typedef struct
 {
-	I2C_HandleTypeDef *i2c_handle;			/* I2C handle */
-	uint8_t addres;					/* Address of DHT20 sensor */
-	uint8_t status_word;				/* First byte received to check if it's equal to 0x18 */
-	uint8_t readBuffer[6];				/* 6 bytes for temperature and humidity */
-	uint32_t data;					/* Raw data for temperature and humidity */
-	float temperature;				/* Temperature value */
-	float humidity;					/* Humidity value */
+	I2C_HandleTypeDef *i2c_handle;		/* I2C handle */
+	uint8_t addres;				/* Address of DHT20 sensor */
+	uint8_t status_word;			/* First byte received to check if it's equal to 0x18 */
+	uint8_t readBuffer[6];			/* 6 bytes for temperature and humidity */
+	uint32_t data;				/* Raw data for temperature and humidity */
+	float temperature;			/* Temperature value */
+	float humidity;				/* Humidity value */
 } DHT20_t;
 
 void dht20_init(DHT20_t *sensor, I2C_HandleTypeDef *i2c);
 void dht20_deinit(void);
 
-static void dht20_write(DHT20_t *sensor, uint8_t *data, uint16_t size);
-static void dht20_read(DHT20_t *sensor, uint8_t *rBuffer, uint16_t size);
+static HAL_StatusTypeDef dht20_write(DHT20_t *sensor, uint8_t *data, uint16_t size);
+static HAL_StatusTypeDef dht20_read(DHT20_t *sensor, uint8_t *rBuffer, uint16_t size);
 
-void dht20_check_status_word(DHT20_t *sensor);
-uint8_t dht20_read_measurement(DHT20_t *sensor);
+HAL_StatusTypeDef dht20_check_status_word(DHT20_t *sensor);
+HAL_StatusTypeDef dht20_read_measurement(DHT20_t *sensor);
 void dht20_parse_data(DHT20_t *sensor);
 
 uint32_t humidity_calculation(DHT20_t *sensor);
@@ -52,6 +52,6 @@ uint32_t temperature_calculation(DHT20_t *sensor);
 float humidity_formula(DHT20_t *sensor);
 float temperature_formula(DHT20_t *sensor);
 
-void dht20_sensor_read(DHT20_t *sensor);
+HAL_StatusTypeDef dht20_sensor_read(DHT20_t *sensor);
 
 #endif /* DHT20_INC_DHT20_H_ */
